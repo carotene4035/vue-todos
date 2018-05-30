2ff16da4419ee0ccb1a0b13986ee23dcbb8af690
carotene512 <karoten512@gmail.com>
2018-05-30 21:08:39 +0900

    formの追加。

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        form
+      <input type="todoName"><button v-on:click='submit'>登録する</button>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      todoName: ''
+    }
+  },
+  methods: {
+    submit: function () {
+    }
+  }
+}
+</script>
