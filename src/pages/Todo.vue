78a3023c706f4529f06bd467868cbd25f805abed
carotene512 <karoten512@gmail.com>
2018-05-30 20:16:16 +0900

    コンテナComponent、プレゼンタComponentの作成。

---
+++ src/pages/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <todo-form></todo-form>
+      <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import TodoForm from '@/components/TodoForm'
+
+export default {
+  components: { TodoList, TodoForm }
+}
+</script>
+
+<style scoped>
+
+</style>
