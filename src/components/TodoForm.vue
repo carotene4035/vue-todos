14fd7660b6bda9f5fc09fd314111f2f56a79c4f9
carotene512 <karoten512@gmail.com>
2018-05-30 21:46:19 +0900

    vuexの導入。mutationsの実験。

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,7 +1,13 @@
 <template>
+  <div>
+    <input type="todoName">
+    <button v-on:click='submit'>提出する</button>
+    <div>{{ todos }}</div>
     <div>
-      <input type="todoName"><button v-on:click='submit'>登録する</button>
+      <p>{{ count }}</p>
+      <button v-on:click='increment'>カウントする</button>
     </div>
+  </div>
 </template>
 
 <script>
@@ -11,8 +17,26 @@ export default {
       todoName: ''
     }
   },
+  computed: {
+    count () {
+      return this.$store.state.count
+    },
+    todos () {
+      return this.$store.state.todos
+    }
+  },
   methods: {
-    submit: function () {
+    increment () {
+      /** これだと、対象のストアがわかりにくい */
+      // 引数を渡すこともできる
+      // this.$store.commit('increment', 2)
+
+      // オブジェクトとして渡すこともできる（store側ではpayloadとして渡される
+      this.$store.commit('increment', { amount: 2 })
+    },
+    submit () {
+      /** これだと、対象のストアがわかりにくい */
+      this.$store.commit('addtodo')
     }
   }
 }
---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  /** 状態 */
+  state: {
+    count: 0,
+    /** 違う種類のobjectは同じstateに管理していいのか？実験 */
+    todos: []
+  },
+  /*
+   * 状態を変更する
+   */
+  mutations: {
+    /** あとでactionを使って書き直し */
+    increment (state, payload) {
+      state.count = state.count + payload.amount
+    },
+    addtodo (state, todo) {
+      state.todos.push(todo)
+    }
+  },
+  /*
+   * storeの値をfilteringしたりカウントしたりするとき、getternにまとめる
+   */
+  getters: {
+
+  }
+})
+
+export default store
