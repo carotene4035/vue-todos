9a261eb0f2e3f45846b7606a056d3aac691ed99e
carotene512 <karoten512@gmail.com>
2018-06-02 15:58:40 +0900

    mastdn構築に必要な大体のComponentを生成。

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <todo-form></todo-form>
+      <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import MyFavorites from '@/components/Index/MyFavorites.vue'
+import MyToots from '@/components/Index/MyToots.vue'
+import Timeline from '@/components/Index/Timeline.vue'
+import TootForm from '@/components/Index/TootForm.vue'
+
+export default {
+  components: { MyFavorites, MyToots, Timeline, TootForm }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Todo from '@/pages/Todo'
+import Index from '@/pages/Index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/todos',
       name: 'Todo',
       component: Todo
+    },
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
     }
   ]
 })
